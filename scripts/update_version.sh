4518fc2cd3c5e72e7a3bc358c268b20d8ee85ee7
Phạm Văn Nguyên <npv2k1@gmail.com>
2025-12-27 22:53:58 +0700

    chore: bump version to 1.0.2

---
+++ scripts/update_version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Usage: ./update_version.sh <new_version>
+# FORMAT IS <0.0.0>
+
+if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  find . -name 'package.json' -not -path '*/node_modules/*' -exec bash -c '
+    # Parse the version from package.json
+    current_version=$(grep -o "\"version\": \"[^\"]*" "$0" | cut -d"\"" -f4)
+
+    # Update the version
+    perl -i -pe"s/$current_version/'$1'/" "$0"
+  '  {} \;
+
+  echo "Updated versions to $1";
+  
+  # Create git tag for this version
+  git add .
+  git commit -m "chore: bump version to $1"
+  git tag -a "v$1" -m "Release version $1"
+  
+  echo "Created git tag v$1";
+  echo "Push with: git push && git push --tags";
+else
+  echo "Version format <$1> isn't correct, proper format is <0.0.0>";
+fi
